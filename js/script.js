209e882a70d9e28c1c7f64891d969effeb2c1138
jinnyjkim <156854075+jinnyjkim@users.noreply.github.com>
2024-03-26 16:49:05 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,67 @@
+let pokemonList = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Pidgeotto","Pidgeot","Rattata","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran","Nidorina","Nidoqueen","Nidoran","Nidorino","Nidoking","Clefairy","Clefable","Vulpix","Ninetales","Jigglypuff","Wigglytuff","Zubat","Golbat","Oddish","Gloom","Vileplume","Paras","Parasect","Venonat","Venomoth","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine","Poliwag","Poliwhirl","Poliwrath","Abra","Kadabra","Alakazam","Machop","Machoke","Machamp","Bellsprout","Weepinbell","Victreebel","Tentacool","Tentacruel","Geodude","Graveler","Golem","Ponyta","Rapidash","Slowpoke","Slowbro","Magnemite","Magneton","Farfetch'd","Doduo","Dodrio","Seel","Dewgong","Grimer","Muk","Shellder","Cloyster","Gastly","Haunter","Gengar","Onix","Drowzee","Hypno","Krabby","Kingler","Voltorb","Electrode","Exeggcute","Exeggutor","Cubone","Marowak","Hitmonlee","Hitmonchan","Lickitung","Koffing","Weezing","Rhyhorn","Rhydon","Chansey","Tangela","Kangaskhan","Horsea","Seadra","Goldeen","Seaking","Staryu","Starmie","Mr. Mime","Scyther","Jynx","Electabuzz","Magmar","Pinsir","Tauros","Magikarp","Gyarados","Lapras","Ditto","Eevee","Vaporeon","Jolteon","Flareon","Porygon","Omanyte","Omastar","Kabuto","Kabutops","Aerodactyl","Snorlax","Articuno","Zapdos","Moltres","Dratini","Dragonair","Dragonite","Mewtwo","Mew"];
+
+// Answers
+
+// Question 1
+function displayName () {
+    document.querySelector("#host-name").innerHTML = "Jinny";
+    document.querySelector("#collaborator-name").innerHTML = "Jahnavi";
+}
+
+// Question 2
+// You can also create their own CSS classes in style.css file and use that. 
+
+
+
+
+// Question 3
+// Try change event first
+// There is another event which fires for every character. This is what we want to use.
+function autopopulate(){
+    if (document.querySelector("#first_name").value == "John") {
+        document.querySelector("#last_name").value = "Doe"
+    }
+}
+
+
+// Question 4
+// Try change event first
+// There is another event which fires for every character. This is what we want to use.
+
+
+
+// Question 5
+// Try change event first
+// There is another event which fires for every character. This is what we want to use.
+function passwords () {
+    if (document.querySelector("#password").value == document.querySelector("#confirm_password").value) {
+        document.querySelector("#password-alert").innerHTML = "Password Matches";
+        console.log("hi")
+    }
+    else {
+        document.querySelector("#password-alert").innerHTML = "Password does not match";
+    }
+}
+
+
+
+//Question 6
+
+
+
+
+
+//Question 7 
+
+
+
+
+
+//Question 8
+
+
+
+
+
+//Question 9 
+
